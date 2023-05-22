a56c2e6aca622b044d013420c76ba44de92151b6
zhenyuen <zhenyuen.dev@gmail.com>
2023-05-22 21:24:11 +0100

    feat: double clock freq

--- processor/toplevel.v
+++ processor/toplevel.v
@@ -55,7 +55,7 @@ module top (led);
 	/*
 	 *	Use the iCE40's hard primitive for the clock source.
 	 */
-	SB_HFOSC #(.CLKHF_DIV("0b11")) OSCInst0 (
+	SB_HFOSC #(.CLKHF_DIV("0b10")) OSCInst0 (
 		.CLKHFEN(ENCLKHF),
 		.CLKHFPU(CLKHF_POWERUP),
 		.CLKHF(clk)
